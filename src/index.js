e4b250bb187885d5759d835602493b1cb425d7be
Алексей <fortuk22@rambler.ru>
2022-07-01 01:30:21 +0300

    изменения

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store.store}>
+        <Provider store={store}>
             {/* {<PersistGate loading={null} persistor={store.persistor}> */}
             <App />
             {/* {</PersistGate> */}
