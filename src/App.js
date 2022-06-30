61383df36cd703ba24f6087042cfc651c1e53588
Алексей <fortuk22@rambler.ru>
2022-07-01 01:23:39 +0300

    пропись изменения плагинов

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import './App.module.css';
+import s from './App.module.css';
 import ContactForm from './components/ContactForm'
 import ContactList from './components/ContactList'
 import Filter from './components/Filter'
